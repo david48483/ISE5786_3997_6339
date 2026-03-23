363a0d7961046ac562ef5d4c753058ebfccb6e7e
Udi Lipshitsz <udilipsh@gmail.com>
2026-03-23 18:14:47 +0200

    בדיקות מישור, צריך תיעוד עדיין

--- unittests/geometries/PlaneTests.java
+++ unittests/geometries/PlaneTests.java
@@ -3,14 +3,67 @@ package geometries;
 import geometries.impl.Plane;
 import org.junit.jupiter.api.Test;
 import primitives.Point;
+import primitives.Vector;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PlaneTests {
 
+    double DELTA = 1e-6;
+
+    Point p2 = new Point(1, 0, 0);
+    Point p3 = new Point(0, 1, 0);
+
+    Vector v1 = new Vector(3, 4, 5);
+
     @Test
     void testConstructor() {
-        assertEquals(12, new Plane(new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 0, 0)),
-                "ERROR: Plane constructor from three points did not create the expected plane");
+
+        // ============ Equivalence Partitions Tests ==============
+
+        //TC01 check constructor
+        assertDoesNotThrow(() -> new Plane(Point.ZERO, p2, p3),
+                "ERORR,Plane constructor failed to create the expected plane  ");
+
+        // =============== Boundary Values Tests ==================
+
+        assertThrows(IllegalArgumentException.class, () -> new Plane(p2, p2, p3), "aaaa");
+
+        assertThrows(IllegalArgumentException.class, () -> new Plane(p2, p3, p2), "aaaa");
+
+        assertThrows(IllegalArgumentException.class, () -> new Plane(p3, p2, p2), "aaaa");
+
+        assertThrows(IllegalArgumentException.class, () -> new Plane(p2, new Point(4, 0, 0), Point.ZERO),
+                "aaaa");
+
     }
+
+    @Test
+    void testConstructurB() {
+        Plane plane = new Plane(p2, v1);
+        assertEquals(1, plane.getNormal(p2).length(), DELTA, "aaa");
+
+    }
+
+    @Test
+    void testGetNormal() {
+        // הגדרת נתוני הבסיס למישור - מישור XY
+
+        Plane plane = new Plane(Point.ZERO, p2, p3);
+
+        Vector expectedNormal = new Vector(0, 0, 1);
+
+        // ============ Equivalence Partitions Tests ==============
+
+        // TC01: A point that is not the reference
+        assertEquals(expectedNormal, plane.getNormal(p2),
+                "getNormal() wrong result for a point on the plane (not reference point)");
+
+        // =============== Boundary Values Tests ==================
+
+        // TC10: the reference point
+        assertEquals(expectedNormal, plane.getNormal(Point.ZERO),
+                "getNormal() wrong result for the reference point of the plane");
+    }
+
 }
