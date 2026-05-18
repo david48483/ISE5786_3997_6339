5f097b1747debdba4e2918f670e4d6ad97d1b3ca
Udi Lipshitsz <udilipsh@gmail.com>
2026-05-18 18:44:10 +0300

    chek intergration- sphere

--- unittests/renderer/CameraIntersectionIntegration.java
+++ unittests/renderer/CameraIntersectionIntegration.java
@@ -37,8 +37,39 @@ public class CameraIntersectionIntegration {
     @Test
     void testCameraRaySphereIntegration() {
 
-        Sphere sphere = new Sphere(Point.ZERO, 1);
-        assertIntersectionsCount(cam1, sphere, 9, "Camera-Ray-Sphere Integration Test");
+        Point point = new Point(0, 0, -3);
+
+        //sphere: X^2+Y^2+ (Z+3)^2=R^2
+        //ray: P0 + t*V = (0,0,0) + t*(x,y,-1) = (tx, ty, -t)
+        //intersection: (tx)^2 + (ty)^2 + (-t+3)^2 = R^2
+        //t^2(x^2+y^2+1) - 6t + 9 - R^2 = 0
+        //r=x^2+y^2
+        //t^2(r+1) - 6t + 9 - R^2 = 0
+        //2 intersection iff b^2-4ac>0=======    R^2>(9r^2/r^2+1)
+        //(calculate for x,y: (0,0)==0<R^2<(4.5),(0,1)==(4.5)<R^2<(6),(1,1)==R^2>(6):
+
+        // ============ Equivalence Partitions Tests ==============
+
+        // EP01 - Sphere is in front of the camera and intersects with the center rays only (2 points)
+        Sphere sphere = new Sphere(point, 1);
+        assertIntersectionsCount(cam1, sphere, 2, "Camera-Ray-Sphere Integration Test with 2 intersection");
+
+        //EP02 - Sphere is in front of the camera and intersects with all rays (18 points)
+        sphere = new Sphere(point, 2.5);
+        assertIntersectionsCount(cam1, sphere, 18, "Camera-Ray-Sphere Integration Test with 18 intersection");
+
+        //  EP03 - Sphere is in front of the camera and intersects with some rays (10 points)
+        sphere = new Sphere(point, 2.2);
+        assertIntersectionsCount(cam1, sphere, 10, "Camera-Ray-Sphere Integration Test with 10 intersection");
+
+        //  EP04 - Sphere is behind the camera (0 points)
+        sphere = new Sphere(new Point(0, 0, 4), 1);
+        assertIntersectionsCount(cam1, sphere, 0, "Camera-Ray-Sphere Integration Test with sphere behind the camera");
+
+        //  EP05 - Camera is inside the sphere (9 points)
+        sphere = new Sphere(point, 3);
+        assertIntersectionsCount(cam1, sphere, 9, "Camera-Ray-Sphere Integration Test  with camera inside the sphere");
+
     }
 
     @Test
