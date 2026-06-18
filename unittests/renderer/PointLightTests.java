837d188f5197a037698a852443682313e8c520ed
דוד דוידוביץ <david48483@gmail.com>
2026-06-18 17:49:38 +0300

    Refactor intersection handling in geometries: introduce Intersection class and update findIntersections methods

--- unittests/renderer/PointLightTests.java
+++ unittests/renderer/PointLightTests.java
@@ -12,6 +12,7 @@ public class PointLightTests {
 
     @Test
     void TestGetL() {
+
         PointLight light = new PointLight(new Color(100, 100, 100), Point.ZERO);
 
         // ============ Equivalence Partitions Tests ==============
@@ -30,22 +31,21 @@ public class PointLightTests {
         Color base = new Color(100, 100, 100);
 
         // ============ Equivalence Partitions Tests ==============
-        // TC11: Only constant attenuation -> no distance effect.
+        // EP01: Only constant attenuation -> no distance effect.
         PointLight onlyKc = new PointLight(base, Point.ZERO).setKc(1.0).setKl(0.0).setKq(0.0);
         assertEquals(base, onlyKc.getIntensity(new Point(10, 0, 0)),
                 "With only kC=1, intensity should remain unchanged");
 
-        // TC12: Linear attenuation.
+        // EP02: Linear attenuation.
         PointLight linear = new PointLight(base, Point.ZERO).setKc(1.0).setKl(0.5).setKq(0.0);
         // d=2 => attenuation = 1 + 0.5*2 = 2 -> scale 0.5
         assertEquals(base.scale(0.5), linear.getIntensity(new Point(2, 0, 0)),
                 "Linear attenuation should reduce intensity by expected factor");
 
         // =============== Boundary Values Tests ==================
-        // TC13: Quadratic attenuation with very close point.
-        // מקרה קצה של הנקודה במיקום של המנורה צריך לחזור ללא מקדם ההנחתה
-        assertEquals(Vector.AXIS_X, light.getL(new Point(2, 0, 0)),
-                "getL should return normalized vector from light to point");
+        // VB01: Quadratic attenuation with very close point.
+        assertEquals(base, linear.getIntensity(Point.ZERO),
+                "With only kC=1, intensity should remain unchanged");
 
 
     }
